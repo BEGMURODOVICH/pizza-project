5970c5d8e4f8f2612cc992c2ab778b77f6c6ead0
Azizbek Begmurodovich Sayfiyev <azizbekofficialaccaunt@gmail.com>
2024-12-02 13:52:15 +0500

    test

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ const menuBtn = document.querySelector(".menu-btn");
 
 menuBtn.addEventListener("click", () => {
   modalAccaount.classList.toggle("modal-accaunt-active");
+  if (menuBtn.src.includes("Menu.svg")) {
+    menuBtn.src = "img/close.svg";
+  } else {
+    menuBtn.src = "img/Menu.svg";
+  }
 });
