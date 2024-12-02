3b065ac65e0868f4f2ed41929be33094d84d9e66
Azizbek Begmurodovich Sayfiyev <azizbekofficialaccaunt@gmail.com>
2024-12-02 10:11:35 +0500

    modal 1.0

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const modalAccaount = document.querySelector(".modal-accaunt");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  modalAccaount.classList.toggle("modal-accaunt-active");
+});
